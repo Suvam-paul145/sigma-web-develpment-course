d3eda5be0e350607e587a3a1a8cfd6b91778a2bd
Suvam-paul145 <suvampersonal555@gmail.com>
2024-12-20 14:08:21 +0530

    day 70

--- Day 67/DOM.js
+++ Day 67/DOM.js
@@ -1,25 +1,25 @@
-let boxes = document.getElementsByClassName("box");
-console.log(boxes);
+// let boxes = document.getElementsByClassName("box");
+// console.log(boxes);
 
-boxes[2].style.backgroundColor = "red"
+// boxes[2].style.backgroundColor = "red"
 
-document.getElementById("Box1").style.backgroundColor = "yellow"
+// document.getElementById("Box1").style.backgroundColor = "yellow"
 
-document.querySelector(".box").style.color = "blue"
+// document.querySelector(".box").style.color = "blue"
 
-//document.querySelectorAll(boxes).style.backgroundColor = "purple"
+// //document.querySelectorAll(boxes).style.backgroundColor = "purple"
 
 
-console.log(document.querySelectorAll(".box"))
+// console.log(document.querySelectorAll(".box"))
 
-//  document.querySelectorAll(".box").forEach(e =>{
-//  }) 
+// //  document.querySelectorAll(".box").forEach(e =>{
+// //  }) 
 
-// multiple properties in one loop 
-document.querySelectorAll(".box").forEach(e =>
-{
-    e.style.color = "white"
-    e.style.backgroundColor = "green"
+// // multiple properties in one loop 
+// document.querySelectorAll(".box").forEach(e =>
+// {
+//     e.style.color = "white"
+//     e.style.backgroundColor = "green"
 
-}
-)
+// }
+// )
