9d29f887f4c1ea602a99a1544a764dd6bedf7e1e
Suvam-paul145 <suvampersonal555@gmail.com>
2025-01-17 13:12:04 +0530

    add

---
+++ Day 74/script.js
@@ -0,0 +1,11 @@
+let button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+    document.querySelector("div").innerHTML ="  hi <b>clicked</b> ia ma new paragraph"
+})
+
+button.addEventListener("click", () => {
+    alert("i have been clicked ")
+})
+
+button.addEventListener("mouseup" , () =>{P})
